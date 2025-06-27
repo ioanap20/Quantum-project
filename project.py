0e26bfd63dc7ae40b64af962c8b8714c80f1f074
doovashti <156182602+doovashti@users.noreply.github.com>
2025-06-27 13:09:16 +0200

    added Pauli X and Y translation functions

--- project.py
+++ project.py
@@ -2,3 +2,18 @@ def translation():
     print("Test only one branch")
     #this function is for translation
 
+def X_translation():
+    X = '-iX(pi)'
+    return X
+
+X= X_translation()
+print(X)
+
+def Y_translation():
+    Y = '-iZ(pi)X(pi)'
+    return Y
+Y = Y_translation()
+print(Y)
+
+def Z_translation():
+ Z = 
